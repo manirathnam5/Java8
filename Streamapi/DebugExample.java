89c18a612cd36a3a55ba2f9c764419594666b492
manirathnam5 <manirathnam501@gmail.com>
2024-09-19 22:16:36 +0530

    streamapi ex

---
+++ Streamapi/DebugExample.java
@@ -0,0 +1,23 @@
+
+
+public class DebugExample {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+
+        int b  = 10;
+
+        int c = 20;
+
+        int aTimesB = a*b;
+
+        if(aTimesB > c){
+            System.out.println("Bigger");
+        }
+        else{
+            System.out.println("Smaller");
+        }
+
+    }
+}
---
+++ Streamapi/StreamapiExample1.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class StreamapiExample1{
+
+
+
+
+    public static void recursive(){
+        System.out.println("Recursive function ");
+    }
+
+        public static void main(String[] arg){
+
+            List<Integer>    list = Arrays.asList(23,11,44,21,2,8);
+
+
+            list.forEach(System.out::println);
+
+            Stream<Integer>  data = list.stream();
+        }
+
+
+
+
+    public void setAccount(){
+        for (int i1 = 0; i1 < 1; i1++) {
+
+        }
+        //sts
+        //sts
+        //sss
+        //ss
+        System.out.println("Account setting ") ;
+
+        int i = 0;
+
+
+    }
+
+
+}
+
+
+
+
