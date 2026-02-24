67da0a8e2ccb047536c320782b73fc76a5ce6816
manirathnam5 <manirathnam501@gmail.com>
2026-02-24 16:19:38 +0530

    functional interface

---
+++ LambdaExpression/LambdaExp4.java
@@ -0,0 +1,31 @@
+package LambdaExpression;
+
+
+public class LambdaExp4 {
+
+    public static void main(String[] arg) {
+        // Before lambda function
+        Calculator calc = new Calculator() {
+            @Override
+            public int operate(int a, int b) {
+                return a + b;
+            }
+        };
+
+        //Interface implementation using Lambda function
+
+        Calculator lambda = (a,b )-> a+b;
+
+        int result = lambda.operate(32, 34);
+
+        System.out.println(result);
+    }
+
+
+}
+
+
+interface Calculator {
+
+    int operate(int a, int b);
+}
