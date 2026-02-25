c485dc06ad42b3298368a65be3c766bf7e6aa668
manirathnam5 <manirathnam501@gmail.com>
2026-02-25 22:14:59 +0530

    qa

--- InterviewQuestions/Java8Example.java
+++ InterviewQuestions/Java8Example.java
@@ -1,4 +1,4 @@
-
+ package InterviewQuestions;
 
 class Java8Example{
 
--- InterviewQuestions/Java8InterviewQuestion1.java
+++ InterviewQuestions/Java8InterviewQuestion1.java
@@ -1,13 +1,12 @@
+ package InterviewQuestions;
+
 import java.util.Arrays;
 import java.util.List;
 
-class Java8Example{
-
-
-
+class Java8InterviewQuestion1{
 
    //static
-    void printElement(int i){
+    static  void printElement(int i){
 
       System.out.println(i);
     
@@ -16,11 +15,11 @@ class Java8Example{
 
     public static void main(String[] arg){
 
-    Java8Example    obj = new Java8Example();
+    Java8Example     obj = new Java8Example();
 
-    List<Integer>  list = Arrays.asList(5,4,7,3,8,12,13,14,11,23,10);
+    List<Integer>    list = Arrays.asList(5,4,7,3,8,12,13,14,11,23,10);
 
-    list.stream().filter(a-> a >= 6).sorted().forEach(obj::printElement);
-        
+    //list.stream().filter(a-> a >= 6).sorted().forEach(obj::printElement);
+       printElement(10);
     }
 }
