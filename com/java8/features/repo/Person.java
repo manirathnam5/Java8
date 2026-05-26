c032ae6aab6ba9c8f02829fac8fb25319a69048a
manirathnam5 <manirathnam501@gmail.com>
2026-05-26 13:42:51 +0530

    added pojo's for Address & Person

---
+++ com/java8/features/repo/Person.java
@@ -0,0 +1,28 @@
+package com.java8.features.repo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Person {
+
+    private String name;
+
+    private int height;
+
+    private double salary;
+
+    private String gender;
+
+    private int kids;
+
+    List<String> hobbies = new ArrayList<>();
+
+    Optional<Address> address;
+
+
+
+
+
+
+}
