c63ea51ba96d4368f11d34154b3f169d3b9c83f4
manirathnam5 <manirathnam501@gmail.com>
2026-05-26 14:15:05 +0530

    added constructors

--- com/java8/features/repo/Person.java
+++ com/java8/features/repo/Person.java
@@ -21,8 +21,73 @@ public class Person {
     Optional<Address> address;
 
 
+    public Person() {
+        super();
+    }
 
+    public Person(String name, int height, double salary, String gender, int kids, List<String> hobbies, Optional<Address> address) {
+        this.name = name;
+        this.height = height;
+        this.salary = salary;
+        this.gender = gender;
+        this.kids = kids;
+        this.hobbies = hobbies;
+        this.address = address;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getKids() {
+        return kids;
+    }
+
+    public void setKids(int kids) {
+        this.kids = kids;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public Optional<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Optional<Address> address) {
+        this.address = address;
+    }
 }
