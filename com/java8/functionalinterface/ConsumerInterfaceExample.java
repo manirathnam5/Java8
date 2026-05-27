06669f4c828079dc3a3708f817169925e85c0a00
manirathnam5 <manirathnam501@gmail.com>
2026-05-27 20:23:34 +0530

    consumer interface changes

--- com/java8/functionalinterface/ConsumerInterfaceExample.java
+++ com/java8/functionalinterface/ConsumerInterfaceExample.java
@@ -7,29 +7,44 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerInterfaceExample {
-    private static Consumer<Person>  c3 = s->System.out.println(s.getName().toUpperCase());
 
-    private static   Consumer<Person>   c4 = s -> {
-        if(s.getGender().equalsIgnoreCase("Male")){
+    private static Consumer<Person> c1 = s -> System.out.println(s);
+    private static Consumer<Person> c2 = s -> System.out.println(s.getName().toUpperCase());
+    private static Consumer<Person> c3 = s -> System.out.println(s.getName().toUpperCase());
+
+    private static Consumer<Person> c4 = s -> {
+        if (s.getGender().equalsIgnoreCase("Male")) {
             System.out.println(s.getName());
         }
     };
 
+    static List<Person> personList = PersonRepository.getAllPersons();
+
+
+    private static void printPersonWithCondition() {
+        personList.forEach(s -> {
+            if (s.getGender().equals("Male") && s.getHeight() >= 140) {
+                c1.andThen(c2).accept(s);
+            }
+        });
+
+    }
+
 
     public static void main(String[] arg) {
+        // example for String type
 
-       // example for String type
-
-        Consumer<String> c1 = s -> System.out.println(s + 10);
+        Consumer<String> c1 = s -> System.out.println(s);
         Consumer<String> c2 = s -> System.out.println(s.toUpperCase());
         c1.andThen(c2).accept("manish");
 
         //example for Person type data
 
         System.out.println("Person data ... ");
-        List<Person>  personList = PersonRepository.getAllPersons();
 
-        personList.forEach(c4);
+        //personList.forEach(c4);
+        printPersonWithCondition();
 
     }
+
 }
