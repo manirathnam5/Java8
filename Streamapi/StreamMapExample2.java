f8665b27376df5f93046db0c13959ceab4c80cad
manirathnam5 <manirathnam501@gmail.com>
2026-05-30 00:20:09 +0530

    map example with realtime data

--- Streamapi/StreamMapExample2.java
+++ Streamapi/StreamMapExample2.java
@@ -4,19 +4,23 @@ import com.java8.features.repo.Person;
 import com.java8.features.repo.PersonRepository;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class StreamMapExampe2 {
+public class StreamMapExample2 {
 
+
+    static Function<Person, String> s2 = s -> s.getName().toLowerCase();
 
     public static void main(String[] arg) {
 
         List<Person> personList = PersonRepository.getAllPersons();
 
         List<String> s1 = personList.stream()
-                .map((person) -> person.getName())
-                .map(s -> s.toUpperCase())
+                .map((s2))
+                // .map(s -> s.toUpperCase())
                 .collect(Collectors.toList());
+
         System.out.println(s1);
 
     }
