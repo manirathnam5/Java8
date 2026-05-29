ac46aaf8f430b37215caeaab5dd187e5c586a552
manirathnam5 <manirathnam501@gmail.com>
2026-05-30 01:11:09 +0530

    flatmap example2 realtime data

---
+++ Streamapi/StreamFlatMapExample2.java
@@ -0,0 +1,26 @@
+package Streamapi;
+
+import com.java8.features.repo.Person;
+import com.java8.features.repo.PersonRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFlatMapExample2 {
+
+    static List<String> showHobbies(List<Person> personList) {
+
+        return personList.stream()
+                .map(personList1 -> personList1.getHobbies())
+                .flatMap(hobbiesList -> hobbiesList.stream())
+                .distinct()
+                .collect(Collectors.toList());
+
+    }
+
+
+    public static void main(String[] arg) {
+
+        System.out.println(showHobbies(PersonRepository.getAllPersons()));
+    }
+}
