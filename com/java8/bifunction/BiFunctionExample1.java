11bb2b75bd613ac027e18f375db8a5448648ae72
manirathnam5 <manirathnam501@gmail.com>
2026-05-29 20:05:27 +0530

    Bifunction example

--- com/java8/bifunction/BiFunctionExample1.java
+++ com/java8/bifunction/BiFunctionExample1.java
@@ -1,4 +1,21 @@
 package com.java8.bifunction;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class BiFunctionExample1 {
+
+
+    private static BiFunction<String, String, Integer> biFunction = (s, s2) -> s.concat(s2).length();
+
+    private static Function<String, Integer> function = (str) -> str.length();
+
+
+    public static void main(String[] arg) {
+
+        System.out.println(biFunction.apply(" Manish ", " Manirathnam R"));
+
+        System.out.println(function.apply("SamsclubWalmart"));
+
+    }
 }
