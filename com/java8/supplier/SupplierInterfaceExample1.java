f2eaba683e06bc18c186e7234d4b52479ca422eb
manirathnam5 <manirathnam501@gmail.com>
2026-05-29 13:40:25 +0530

    supplier interface example

---
+++ com/java8/supplier/SupplierInterfaceExample1.java
@@ -0,0 +1,28 @@
+package com.java8.supplier;
+
+import com.java8.features.repo.Person;
+import com.java8.features.repo.PersonRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierInterfaceExample1 {
+
+    private static Supplier<List<Person>> supplier1 = () -> PersonRepository.getAllPersons();
+
+    private static Supplier<Person> supplier2 = () -> PersonRepository.getPerson();
+
+    static Supplier<List<Integer>> supplier3 = () -> Arrays.asList(2, 4, 5);
+
+
+    public static void main(String[] arg) {
+
+        System.out.print(supplier1.get());
+
+        System.out.println(supplier2.get());
+
+        System.out.println(supplier3.get());
+
+    }
+}
