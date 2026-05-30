f71f5c1e2867e51f43993dc5606db44801b9c963
manirathnam5 <manirathnam501@gmail.com>
2026-05-30 11:23:04 +0530

    filter example

--- Streamapi/StreamFilterExample1.java
+++ Streamapi/StreamFilterExample1.java
@@ -3,8 +3,10 @@ package Streamapi;
 import com.java8.features.repo.Person;
 import com.java8.features.repo.PersonRepository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamFilterExample1 {
 
@@ -16,10 +18,12 @@ public class StreamFilterExample1 {
                 .filter((person -> person.getName().length() > 5))
                 .map(person -> person.getName())
                 .collect(Collectors.toList());
-
-
         System.out.println(names);
 
+        List<String> names2 = Arrays.asList("Sam", "Anderson", "Peter", "Nancy");
+        /*Stream<String> s1 =*/
+        names2.stream().filter(s -> s.length() > 3)
+                .forEach((s) -> System.out.println(s));
 
     }
 }
