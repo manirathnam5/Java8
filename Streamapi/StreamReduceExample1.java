8501d96a11e69e4ff1d02a1df22bd79fb370a4bf
manirathnam5 <manirathnam501@gmail.com>
2026-05-30 14:43:03 +0530

    stream reduce example

--- Streamapi/StreamReduceExample1.java
+++ Streamapi/StreamReduceExample1.java
@@ -3,8 +3,13 @@ package Streamapi;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
 
 public class StreamReduceExample1 {
+
+    static BinaryOperator<Integer> bo1 = (a, b) -> a * b;
+    static BinaryOperator<Integer> bo2 = (a, b) -> a + b;
+    static BinaryOperator<Integer> bo3 = (a, b) -> a - b;
 
     public static void main(String[] arg) {
 
@@ -12,7 +17,7 @@ public class StreamReduceExample1 {
 
         Optional<Integer> sum = arrayList.stream().reduce((a, b) -> a + b);
 
-        int sum3 =  arrayList.stream()
+        int sum3 = arrayList.stream()
                 .mapToInt(s -> s)
                 .sum();
 
@@ -20,5 +25,14 @@ public class StreamReduceExample1 {
         System.out.println(sum.get());
         System.out.println(sum3);
 
+
+        int m1 = arrayList.stream().reduce(1, bo1);
+        int m2 = arrayList.stream().reduce(1, bo2);
+        int m3 = arrayList.stream().reduce(0, bo3);
+
+
+        System.out.println(m1);
+        System.out.println(m2);
+        System.out.println(m3);
     }
 }
