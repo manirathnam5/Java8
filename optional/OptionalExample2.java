aaf6f3b3619b0b179ff061f16359dc686b939615
manirathnam5 <manirathnam501@gmail.com>
2026-06-02 23:51:32 +0530

    optional example2

--- optional/OptionalExample2.java
+++ optional/OptionalExample2.java
@@ -4,20 +4,17 @@ import java.util.Optional;
 
 public class OptionalExample2 {
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
 
-        Optional<String>  optional = Optional.ofNullable("manish");
+        Optional<String> optional = Optional.ofNullable("manish");
 
-        Optional<String>  of = Optional.of("mmr");
+        Optional<String> of = Optional.of("mmr");
 
-        System.out.println(optional.isPresent() ? optional.get()  : optional.empty() );
+        System.out.println(optional.isPresent() ? optional.get() : optional.empty());
 
+        System.out.println(optional.isPresent() ? of.get() : of.empty());
 
-         System.out.println(optional.isPresent() ? of.get()  : of.empty() );
-
-        //System.out.println(optional.get());
-
-
+        System.out.println(optional.get());
 
     }
 }
