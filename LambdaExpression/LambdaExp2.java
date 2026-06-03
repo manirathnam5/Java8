a2d71b6add2994b2f3ce646347e4ff13848568cf
manirathnam5 <manirathnam501@gmail.com>
2026-06-03 22:33:38 +0530

    lambda expr ex.

--- LambdaExpression/LambdaExp2.java
+++ LambdaExpression/LambdaExp2.java
@@ -9,14 +9,14 @@ import java.util.*;
 
       ArrayList<Integer>    list = new  ArrayList<Integer>();
 
-     list.add(21);
-     list.add(10);
-     list.add(30);
-    list.add(2);
-    list.add(3);
-    list.add(6);
-    list.add(5);
-    list.add(29);
+      list.add(21);
+      list.add(10);
+      list.add(30);
+      list.add(2);
+      list.add(3);
+      list.add(6);
+      list.add(5);
+      list.add(29);
 
 
     list.forEach( s-> System.out.println(s));
