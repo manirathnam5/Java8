bbc45ae958180fcfdd814e721cb81087c663e65e
manirathnam5 <manirathnam501@gmail.com>
2026-06-20 22:41:19 +0530

    predicate example

--- com/java8/predicatefunctionalinterface/ConsumerPredicateExample.java
+++ com/java8/predicatefunctionalinterface/ConsumerPredicateExample.java
@@ -9,25 +9,20 @@ import java.util.function.BiPredicate;
 
 public class ConsumerPredicateExample {
 
+    static BiPredicate<Integer, String> heightAndgender = (height, gender) -> height >= 140 && gender.equals("Male");
+
+    static BiConsumer<String, List<String>> biConsumer = (name, hobbies) ->
+            System.out.println(" Name : " + name + " /Hobbies : " + hobbies);
 
 
+    public static void main(String[] arg) {
 
-    static BiPredicate<Integer,String> heightAndgender = (height,gender) -> height>=140 && gender.equals("Male");
-
-    static BiConsumer<String, List<String>> biConsumer = (name , hobbies) ->
-             System.out.println(" Name : " + name + " /Hobbies : " + hobbies);
-
-
-
-    public static void main(String[] arg){
-
-   List<Person>  personList = PersonRepository.getAllPersons();
-
-   personList.forEach(person -> {
-        if(heightAndgender.test(person.getHeight(),person.getGender())){
-            biConsumer.accept(person.getName(),person.getHobbies());
-        }
-   });
+        List<Person> personList = PersonRepository.getAllPersons();
+        personList.forEach(person -> {
+            if (heightAndgender.test(person.getHeight(), person.getGender())) {
+                biConsumer.accept(person.getName(), person.getHobbies());
+            }
+        });
 
 
     }
